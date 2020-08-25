9c0d7a5736cfaa1d586c33f90d553113542bcb97
Manoah <149895ja@gmail.com>
2020-08-25 16:10:26 +0200

    Updates

---
+++ precache-manifest.081696a801dbae07a7669141de6f8a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738489cbd0d377565147aefb7142b365",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "7470bb555f8af85e6eb8",
+    "url": "/todo-app/static/css/main.84d0d772.chunk.css"
+  },
+  {
+    "revision": "8018969a77cf809c06e5",
+    "url": "/todo-app/static/js/2.45a41d46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.45a41d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7470bb555f8af85e6eb8",
+    "url": "/todo-app/static/js/main.92e1d2d5.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
